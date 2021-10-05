97fe05e81524c4ad270d9e14ad25bf70438e19fc
davidcwh <dcwh12321@gmail.com>
2021-10-05 12:02:12 +0800

    Add UserDashboard page basic

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,20 @@
 import LandingPage from "./pages/LandingPage";
 import { Route, Switch, Redirect } from 'react-router';
+import { useAuth0 } from '@auth0/auth0-react';
+import UserDashboardPage from "./pages/UserDashboardPage";
 
 const App = () => {
-  return (
-    <>
-		<Switch>
-			<Route exact path="/">
-				<LandingPage/>
-			</Route>
-		</Switch>
-	</>
-  );
+	const { isAuthenticated } = useAuth0();
+
+	return (
+		<>
+			<Switch>
+				<Route exact path="/">
+					{isAuthenticated ? <UserDashboardPage/> : <LandingPage/>}
+				</Route>
+			</Switch>
+		</>
+	);
 };
 
 export default App;
--- Frontend/src/pages/UserDashboardPage.js
+++ Frontend/src/pages/UserDashboardPage.js
@@ -1,6 +1,21 @@
+import { useAuth0 } from "@auth0/auth0-react";
+import Header from "../common/Header";
+import Container from "../common/Container";
 
 const UserDashboardPage = () => {
+    const { user } = useAuth0();
+    const { sub, email } = user;
 
+    return (
+        <>
+            <Header/>
+            <Container>
+                Logged in user email: {email}
+                <br/>
+                put user dashboard here
+            </Container>
+        </>
+    )
 }
 
 export default UserDashboardPage;
