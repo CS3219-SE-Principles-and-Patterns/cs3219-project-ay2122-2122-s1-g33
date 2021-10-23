c2aa5829aaf5814eec6805fdf03592e461ef0b4d
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-10-23 21:14:56 +0800

    add code execution events to sessions service

--- Sessions/src/server/io.js
+++ Sessions/src/server/io.js
@@ -1,5 +1,5 @@
 const { getDoc } = require("../database/docsService");
-const { getDocDataFromCache, setCodeDocStr } = require("../database/helpers/cacheDbCalls");
+const { getDocDataFromCache, setCodeDocStr, deleteDocDataFromCache, getCodeExecutionStatus, setCodeExecutionStatus } = require("../database/helpers/cacheDbCalls");
 
 const io = require("socket.io")(process.env.SESSIONS_SOCKET_PORT, {
   cors: {
@@ -10,23 +10,46 @@ const io = require("socket.io")(process.env.SESSIONS_SOCKET_PORT, {
 
 io.on("connection", socket => {
   socket.on("get-document", async (docId, userId) => {
-    const document = await getDoc(docId);
+    let document = await getDocDataFromCache(docId);
     if (!document) {
-      socket.emit("document-not-found", docId);
-      socket.disconnect();
-      return;
+      const {docText} = await getDoc(id);
+      if (!docText) {
+        socket.emit("document-not-found", docId);
+        socket.disconnect();
+        return;
+      } else {
+        document = docText;
+      }
     }
     // Join room based off document ID.
     socket.join(docId);
-    socket.emit("load-document", document.docData);
+    socket.emit("load-document", document);
 
-    socket.on("send-changes", delta => {
-      socket.broadcast.to(docId).emit("receive-changes", delta);
+    socket.on("send-changes", async delta => {
+      const isCodeExecRunning = await getCodeExecutionStatus(docId);
+      if (isCodeExecRunning) {
+        socket.emit("code-still-running")
+      } else {
+        socket.broadcast.to(docId).emit("receive-changes", delta);
+      }
     });
+
+    socket.on("run-code", async (userId, data) => {
+      const isCodeExecRunning = await getCodeExecutionStatus(docId);
+      if (isCodeExecRunning) {
+        socket.emit("code-still-running")
+      } else {
+        socket.broadcast.to(docId).emit("code-execution-start");
+        await setCodeExecutionStatus(docId, true);
+        const res = await executeCode(data);
+        socket.emit("code-execution-end", res.body);
+      }
+    })
 
     socket.on("save-document", async data => {
-      await setCodeDocStr(docId, { id: docId, data });
+      await setCodeDocStr(docId, data);
     });
+  
   });
 
   socket.on("disconnecting", (reason) => {
@@ -37,6 +60,9 @@ io.on("connection", socket => {
   })
 })
 
-io.of("/").adapter.on("leave-room", (room, id) => {
-  console.log(`socket ${id} has left room ${room}`);
-});
+// io.of("/").adapter.on("leave-room", (room, id) => {
+//   if (io.sockets.clients(room).length > 0) {
+//     return;
+//   }
+//   deleteDocDataFromCache(id);
+// });
