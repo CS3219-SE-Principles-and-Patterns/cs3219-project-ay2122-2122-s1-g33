7027ecc81ddfe1a21787f6abc1024baf1208fc57
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-10-30 00:51:02 +0800

    add docker build script and kube yml files for docs and sessions services

---
+++ Sessions/build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -euo pipefail
+cd "$(dirname "$0")"
+
+VERSION="$(git describe --dirty --always)"
+if [[ $VERSION == *-dirty ]]; then
+  # We need to ensure the image is loaded again so we give the image a unique
+  # name from other images based on this dirty commit.
+  VERSION+="-$(head -c 5 < /dev/urandom | base32)"
+fi
+
+uri="asia.gcr.io/peerprogram/sessions:$VERSION"
+
+DOCKER_BUILDKIT=1 docker build \
+	--ssh default \
+	-t "$uri" .
+docker push "$uri"
+
+echo "Pushed new image to: $uri"
