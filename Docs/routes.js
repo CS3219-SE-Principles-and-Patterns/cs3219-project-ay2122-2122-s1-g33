fc32abc0986d3e43064d98a1490ec261cf3d1881
Teh Hao Rui Marcus <50549441+marcusteh1238@users.noreply.github.com>
2021-10-29 15:36:04 +0800

    delete the delete

--- Docs/routes.js
+++ Docs/routes.js
@@ -4,6 +4,5 @@ module.exports = app => {
     app.post('/createDoc', docsController.createDoc),
     app.get('/getDoc/:docId', docsController.getDocByDocId),
     app.get('/getUserDocs/:userId', docsController.getDocsByUserId),
-    app.patch('/patchDocText/:docId', docsController.patchDocText),
-    app.delete('/deleteDoc/:docId', docsController.deleteDoc)
+    app.patch('/patchDocText/:docId', docsController.patchDocText)
 }
