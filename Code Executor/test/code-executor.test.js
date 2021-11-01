64b9ca1e8850576030d4a72990deb9cfcc791861
hua-lun <chuahualun@gmail.com>
2021-11-01 20:54:05 +0800

    Add more tests for code executor

--- Code Executor/test/code-executor.test.js
+++ Code Executor/test/code-executor.test.js
@@ -1,43 +1,86 @@
 const axios = require('axios');
+const codeExecutor = require('../app/routes/code-executor.routes')
+const fs = require('fs')
+const tmp = require('tmp');
 
 test('No errors, prints hello world', () => {
-    axios.post('http://localhost:8080/code-executor', {
-        "code": "print('hello world!')"
-    })
-        .then(function (response) {
-            const output = response.data.output;
-            expect(output).toBe('hello world!');
-        })
-        .catch(error => {
-
-        })
-
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "print('hello world!')")
+    return codeExecutor.runPython(filePath).then(data => {
+        expect(data).toBe('hello world!\n');
+    });
 });
 
-test('No errors, addition, no print statements', () => {
-    axios.post('http://localhost:3000/code-executor', {
-        "code": "1 + 1"
-    })
-        .then(function (response) {
-            const output = response.data.output;
-            expect(output).toBe('2');
-        })
-        .catch(error => {
+test('No errors, addition, no print statements, no outputs', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "1 + 1")
+    return codeExecutor.runPython(filePath).then(data => {
+        expect(data).toBe('');
+    });
+});
 
-        })
+test('Error, invalid indentation of newline to separate', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "answer = 1 + 1\n print(answer)")
+    return codeExecutor.runPython(filePath).then(data => {
+
+    }).catch(error => {
+        expect(error.message).toMatch(/Error/);
+    });
 });
 
 
-test('Errors, no semicolon to separate', () => {
-    axios.post('http://localhost:3000/code-executor', {
-        "code": "answer = 1 + 1\n print(answer)"
-    })
-        .then(function (response) {
-            const output = response.data.output;
-            expect(output).toBe();
-        })
-        .catch(error => {
-
-        })
+test('No Errors, newline to separate', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "answer = 1 + 1\nprint(answer)")
+    return codeExecutor.runPython(filePath).then(data => {
+        expect(data).toBe('2\n');
+    });
 });
 
+
+test('Correct input, get square root of 64, use of import statements -> math', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "import math\nx = math.sqrt(64)\nprint(x)")
+    return codeExecutor.runPython(filePath).then(data => {
+        expect(data).toBe('8.0\n');
+    });
+});
+
+test('Error, correct input, import panda does not work as module not downloaded', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "import panda")
+    return codeExecutor.runPython(filePath).then(data => {
+
+    }).catch(error => {
+        expect(error.message).toMatch(/Error/);
+    });
+});
+
+test('Correct input, with function fibonacci', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "def fib(n):\n if n == 0:\n    return n\n elif n == 1 or n == 2:\n    return 1\n else:\n     return fib(n - 1) + fib(n - 2)\nprint(fib(10))")
+    return codeExecutor.runPython(filePath).then(data => {
+        expect(data).toBe('55\n');
+    });
+});
+
+jest.setTimeout(15000);
+
+test('Error, correct input, with function fibonacci, big input, time exceeded', () => {
+    const tmpobj = tmp.fileSync({ prefix: 'pycode', postfix: '.py' });
+    const filePath = tmpobj.name
+    fs.writeFileSync(filePath, "def fib(n):\n if n == 0:\n    return n\n elif n == 1 or n == 2:\n    return 1\n else:\n     return fib(n - 1) + fib(n - 2)\nprint(fib(100))")
+    return codeExecutor.runPython(filePath).then(data => {
+
+    }).catch(error => {
+        expect(error.message).toBe("Time Limit Exceeded: Code took too long to finish executing")
+    });
+});
