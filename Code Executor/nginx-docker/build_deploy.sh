56ae4dfeb4ddad7cb76d0bf388bdd52e67859748
hua-lun <chuahualun@gmail.com>
2021-11-06 19:50:18 +0800

    Update github actions for cd

--- Code Executor/nginx-docker/build_deploy.sh
+++ Code Executor/nginx-docker/build_deploy.sh
@@ -19,4 +19,4 @@ docker push "$uri"
 echo "Pushed new image to: $uri"
 
 gcloud container clusters get-credentials peer-program-cluster-1 --region=asia-southeast1
-kubectl set image deployment/reverse-proxy-deployment reverse-proxy="$uri"
+kubectl set image deployment/reverse-proxy reverse-proxy="$uri"
