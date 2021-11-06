b43732d8275457e71b57a4376860c67c766b11a9
hua-lun <chuahualun@gmail.com>
2021-11-06 19:39:02 +0800

    Update github actions for cd

--- Code Executor/app/build.sh
+++ Code Executor/app/build.sh
@@ -19,5 +19,5 @@ sudo docker push "$uri"
 
 echo "Pushed new image to: $uri"
 
-gcloud container clusters get-credentials peer-program-cluster-1
+gcloud container clusters get-credentials peer-program-cluster-1 --region=asia-southeast1
 kubectl set image deployment/code-executor code-executor="$uri"
